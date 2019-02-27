d1ea76483102e9fdc94720a304040ac215a52764
bryan perez <42827842+bryanoj98@users.noreply.github.com>
2019-02-27 16:03:50 -0500

    Add files via upload

--- Pagina2_js.js
+++ Pagina2_js.js
@@ -1,3 +1,8 @@
+var imagenes=[];
+var fotos=[];
+imagenes.push(document.getElementById('redi'),document.getElementById('nara'),document.getElementById('ama'),document.getElementById('ver'),document.getElementById('azu'),document.getElementById('blan'));
+fotos.push(document.getElementById('fotoR5'),document.getElementById('fotoR4'),document.getElementById('fotoR3_5'),document.getElementById('fotoR3'),document.getElementById('fotoR2'),document.getElementById('fotoR'));
+
 var slider = document.getElementById("motor");
 var slider2 = document.getElementById("leds");
 var slider3 = document.getElementById("ultraD");
@@ -6,15 +11,40 @@ slider.oninput = function() {
   document.getElementById('velo').innerHTML = this.value;
 }
 slider2.oninput = function() {
+  var hasta=7;
   document.getElementById('inte').innerHTML = this.value;
+  if(Number(this.value)<=17&&Number(this.value)>1)hasta=0;
+  else if(Number(this.value)<=34&&Number(this.value)>17)hasta=1;
+  else if(Number(this.value)<=51&&Number(this.value)>34)hasta=2;
+  else if(Number(this.value)<=68&&Number(this.value)>51)hasta=3;
+  else if(Number(this.value)<=85&&Number(this.value)>68)hasta=4;
+  else if(Number(this.value)<=100&&Number(this.value)>85)hasta=5;
+  else hasta=7;
+  for(var i=0;i<7;i++)
+  {
+    if(i<=hasta&&hasta<7){imagenes[i].removeAttribute("hidden");}
+    else {imagenes[i].setAttribute("hidden","");}
+  }
 }
 slider3.oninput = function() {
   document.getElementById('ultra').innerHTML = this.value;
-  //document.getElementById("caji").style.marginLeft;
   document.getElementById("caji").style.marginLeft= String(this.value)+"%";
 }
 slider4.oninput = function() {
+  var hasta=7;
   document.getElementById('foto').innerHTML = this.value;
+  if(Number(this.value)<=17&&Number(this.value)>=0)hasta=0;
+  else if(Number(this.value)<=34&&Number(this.value)>17)hasta=1;
+  else if(Number(this.value)<=51&&Number(this.value)>34)hasta=2;
+  else if(Number(this.value)<=68&&Number(this.value)>51)hasta=3;
+  else if(Number(this.value)<=85&&Number(this.value)>68)hasta=4;
+  else if(Number(this.value)<=100&&Number(this.value)>85)hasta=5;
+  else hasta=7;
+  for(var i=0;i<7;i++)
+  {
+    if(hasta==i){fotos[i].removeAttribute("hidden");}
+    else {fotos[i].setAttribute("hidden","");}
+  }
 }
 function myFunction() {
   // Get the checkbox
@@ -23,9 +53,14 @@ function myFunction() {
   var checkBox = document.getElementById("chicha");
   if (checkBox.checked == true){
     document.getElementById('presen').innerHTML = '1';
+    document.getElementById('mute').setAttribute("hidden","");
+    document.getElementById('sound').removeAttribute("hidden");
 
   } else {
     document.getElementById('presen').innerHTML = '0';
+    document.getElementById('sound').setAttribute("hidden","");
+    document.getElementById('mute').removeAttribute("hidden");
+
   }
   if (checkBox2.checked == true){
     document.getElementById('finde').innerHTML = '1';
